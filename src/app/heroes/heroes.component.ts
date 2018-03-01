bba3a6202b2a972bc526d28f37c24158e942227a
Adriano Soares (BHX2KFV) <BHX2KFV@UPS.COM>
2018-03-01 16:58:49 -0500

    Abstract hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,22 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
 
   constructor() { }
 
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
   onSelect(hero: Hero) {
     this.selectedHero = hero;
   }
 
+  getHeroes(): void {
+    this.heroes = HeroService.getHeroes();
+  }
+
   ngOnInit() {
+    this.getHeroes();
   }
 }
